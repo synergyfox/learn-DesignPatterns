10352328a46d7b32ca4f5d768860b95bf92cb8ee
SynergyFox <basit.noor81@gmail.com>
2024-07-31 19:43:46 +0500

    Singleton Pattern Example added

---
+++ DesignPatterns/Creational/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational
+{
+    /*** The Singleton Design Pattern is a Creational Design Pattern used to ensure that 
+    a class has only one instance and provides a global point of access to it.In C#, 
+    the Singleton Design Pattern is useful when 
+    we need exactly one instance of a class to coordinate actions across the system.***/
+
+    public sealed class Singleton
+    {
+        private static int Counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+
+            }
+            return instance;
+
+        }
+
+
+        //Constructor
+        private Singleton()
+        {
+            Counter++;
+            Console.WriteLine("Total Objects Created " + Counter.ToString());
+        }
+
+        public void Canlift(string printMsg)
+        {
+            Console.WriteLine(printMsg);
+        }
+
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Singleton
+using DesignPatterns.Creational;
+Singleton seniorBodyBuilder = Singleton.GetInstance();
+seniorBodyBuilder.Canlift("Max 120 KG");
+
+Singleton juniorBodyBuilder = Singleton.GetInstance();
+juniorBodyBuilder.Canlift("Max 60 KG");
+
+Singleton starterBodyBuilder = Singleton.GetInstance();
+starterBodyBuilder.Canlift("Max 10 KG");
+
+Console.ReadLine();
+#endregion
+
