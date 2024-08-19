1b60354497cdb3695985605a5eacf6c3f14efbca
SynergyFox <basit.noor81@gmail.com>
2024-08-19 19:16:42 +0500

    Iterator Pattern Added

---
+++ DesignPatterns/Behavioral/Iterator/Iterator.cs
@@ -0,0 +1,90 @@
+﻿namespace DesignPatterns.Behavioral.Iterator
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Employee(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+    }
+
+    //Concrete Collection
+
+    class EmlployeesCollection : IAbstractCollection
+    {        
+        private List<Employee> listEmployees = new List<Employee>();
+        
+        public Iterator CreateIterator()
+        {
+            return new Iterator(this);
+        }
+
+     
+        public int Count
+        {
+            get { return listEmployees.Count; }
+        }
+     
+        public void AddEmployee(Employee employee)
+        {
+            listEmployees.Add(employee);
+        }
+        
+        public Employee GetEmployee(int IndexPosition)
+        {
+            return listEmployees[IndexPosition];
+        }
+    }
+
+    interface IAbstractCollection
+    {        
+        Iterator CreateIterator();
+    }
+    interface IAbstractIterator
+    {
+        Employee First();
+        Employee Next();
+        bool IsCompleted { get; }
+    }
+
+    class Iterator : IAbstractIterator
+    {        
+        private EmlployeesCollection Collection;
+        private int Current = 0;     
+        private readonly int Step = 1;
+        
+        public Iterator(EmlployeesCollection Collection)
+        {        
+            this.Collection = Collection;
+        }
+        
+        public Employee First()
+        {
+            
+            Current = 0;
+            return Collection.GetEmployee(Current);
+        }        
+        public Employee Next()
+        {
+            
+            Current += Step;
+            if (!IsCompleted)
+            {
+                return Collection.GetEmployee(Current);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        public bool IsCompleted
+        {        
+            get { return Current >= Collection.Count; }
+        }
+    }
+
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,4 +1,4 @@
-﻿#define IntrepreterPattern  // Pattern
+﻿#define IteratorPattern  // Pattern
 #define BehavioralPattern  //Category
 
 #if CreationalPatterns
@@ -333,6 +333,26 @@ Console.WriteLine( context.Expression) ;
 
 
 
+#endif
+
+#if IteratorPattern
+
+using DesignPatterns.Behavioral.Iterator;
+
+EmlployeesCollection collection = new EmlployeesCollection();
+collection.AddEmployee(new Employee("Anurag", 100));
+collection.AddEmployee(new Employee("Pranaya", 101));
+collection.AddEmployee(new Employee("Santosh", 102));
+collection.AddEmployee(new Employee("Priyanka", 103));
+collection.AddEmployee(new Employee("Abinash", 104));
+collection.AddEmployee(new Employee("Preety", 105));
+
+Iterator iterator = collection.CreateIterator();
+Console.WriteLine("Iterating over collection:");
+for (Employee emp = iterator.First(); !iterator.IsCompleted; emp = iterator.Next())
+{
+    Console.WriteLine($"ID : {emp.ID} & Name : {emp.Name}");
+}
 #endif
 
 #endif // End BehavioralPattern
